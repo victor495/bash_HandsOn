c102abc16778d6d7d170c0489bda0c4b44c1b2e9
Victor Lorenzana <victor.lorenzana@uvic.cat>
2018-10-09 12:05:13 -0700

    commit 3 de bash

--- exercises/exercise1.sh
+++ exercises/exercise1.sh
@@ -2,5 +2,13 @@ wget https://github.com/dgarrimar/teaching/raw/master/uvic/AdvBI_2018/data/exerc
 nano TAIR9_mRNA.bed #per veure el contingut
 tar -tf "archiu" #per veure el contingut d'un archiu comprimit
 gunzip  "archiu" #per descomprimirlo
-
+sort TAIR9_mRNA.bed > TAIR9_mRNA_sorted.bed
+wc <(grep + <(grep chr1 TAIR9_mRNA.bed)) #surten 4366 chr1 amb "+" en el strand
+#obtenim la llista del tamany dels primers 10 exons i utilitzant el sort ordenem per el tamany.
+awk '{print$NF}' <(sed 's/,/ /g' <(head -n 10 <(awk '{print$11}' TAIR_mRNA.bed)))
+uniq <(sort -nr <(awk '{print$NF}' <(sed 's/,/ /g' <(head -n 10 <(awk '{print$11}' TAIR_mRNA.bed)))))
+#to get the 10 longest exons del cromosoma 1:
+grep chr1 TAIR9_mRNA.bed | awk '{print$11}' | tr , "\n" | sort -nr | head
+#
+awk '{print$4"\t"$10}' TAIR_mRNA.bed | sort -nr -k2,2 | uniq | head 
 
